e677efe905fb542b01d9437688cf2cadea7f85eb
Vijay Kumar Chauhan <vijayc@mkcl.org>
2018-07-24 16:01:07 +0530

    MQLRequest Access Bug Fixes
    close#22

--- client/app/src/views/Home.vue
+++ client/app/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <h2>{{$t('home.homeTitle')}}</h2>
+    <img src="../assets/mkcl-logo.png">
+    <!-- <h2>{{$t('home.homeTitle')}}</h2> -->
 
     <HelloWorld msg="Welcome to Golang Full Stack Vue App" />
   </div>
