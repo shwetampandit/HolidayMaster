3bf02c245d927bff0f6a6363b9a8c9cc4da43826
Ajit Jagtap <ajitj@mkcl.org>
2018-08-05 20:33:30 +0530

    removed program init

--- servers/server1/app/modules/submodules/submodules.go
+++ servers/server1/app/modules/submodules/submodules.go
@@ -4,5 +4,5 @@ package submodules
 import (
 	_ "GolangFullStack/servers/server1/app/modules/submodules/employee"
 	_ "GolangFullStack/servers/server1/app/modules/submodules/login"
-	_ "GolangFullStack/servers/server1/app/modules/submodules/program"
+	// _ "GolangFullStack/servers/server1/app/modules/submodules/program"
 )
