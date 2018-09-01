7b6d473b8ac55eb11cd76e8a21c84a0fd5cfa457
Vijay Kumar Chauhan <vijayc@mkcl.org>
2018-09-01 15:25:11 +0530

    Vuex modules auto registration
    No more import and adding modules in index.js required.. modules.js will auto register all store available in modules directory

---
+++ client/app/src/store/modules/modules.js
@@ -0,0 +1,20 @@
+// Don't do changes in this file
+// All .js file in modules directory are get registered in vuex/store
+// with first letter of file as upper-case e.g camelCase.js -> CamelCase
+import upperFirst from 'lodash/upperFirst'
+
+const requireModule = require.context('.', false, /\.js$/)
+const modules = {}
+
+requireModule.keys().forEach(fileName => {
+  // Don't register this file as a Vuex module
+  if (fileName === './modules.js') return
+
+  const moduleName = upperFirst(
+    fileName.replace(/(\.\/|\.js)/g, '')
+  )
+  alert(moduleName)
+  modules[moduleName] = requireModule(fileName)
+})
+
+export default modules
