5806c368c7afea265f346a5df07b7bc64be43b47
Suvarna <suvarnar@mkcl.org>
2019-12-30 11:50:44 +0530

    ESlint: template curly spacing rule added
    ESlint: template curly spacing rule added

--- client/app/.eslintrc.js
+++ client/app/.eslintrc.js
@@ -8,8 +8,12 @@ module.exports = {
     "plugin:vue/strongly-recommended"
   ],
   rules: {
+  "template-curly-spacing" : "off",
+    "indent": ["error", 2, {
+      "ignoredNodes": ["TemplateLiteral"]
+    }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     },
   parserOptions: {
     parser: 'babel-eslint'
