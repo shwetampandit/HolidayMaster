9b20194fb1ddefbc81d051b0a0f47617ae09f4dd
Pooja Kolte <poojako@mkcl.org>
2020-01-22 14:35:35 +0530

    VBar added
    VBar added

--- client/app/src/components/common/Sidebar.vue
+++ client/app/src/components/common/Sidebar.vue
@@ -5,19 +5,31 @@
     </div>
 
     <div class="custom-menu">
-      <button type="button" id="sidebarCollapse" class="btn btn-primary" @click="toggleSideBar()">
+      <button
+        type="button"
+        id="sidebarCollapse"
+        class="btn btn-primary"
+        @click="toggleSideBar()"
+      >
         <i class="mdi mdi-menu"></i>
         <span class="sr-only">Toggle Menu</span>
       </button>
     </div>
 
-    <ul>
-      <template v-for="(page, i) in this.$router.options.routes">
-        <li :key="i">
-          <b-link :to="page.path"><i :class="page.meta.icon"></i> {{ page.name}}</b-link>
-        </li>
-      </template>
-    </ul>
+    <div v-bar>  
+      <div class="sidebar-vbar">
+        <ul>
+          <template v-for="(page, i) in this.$router.options.routes">
+            <li :key="i">
+              <b-link :to="page.path"
+                ><i class="sidebar-icon" :class="page.meta.icon"></i>
+                {{ page.name }}</b-link
+              >
+            </li>
+          </template>
+        </ul>
+      </div>     
+    </div>
   </div>
 </template>
 
@@ -27,13 +39,13 @@ export default {
   data: function() {
     return {
       isActive: false
-    }
+    };
   },
   methods: {
-        toggleSideBar() {
-            this.isActive = !this.isActive
-        }
+    toggleSideBar() {
+      this.isActive = !this.isActive;
     }
+  }
 };
 </script>
 
